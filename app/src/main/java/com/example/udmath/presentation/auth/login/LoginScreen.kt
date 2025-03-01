a720e383b7a87d3a3eda8255c5f7a1dcb479a69c
Santiago Alfonso <salfonsop@udistrital.edu.co>
2025-03-01 11:04:58 -0500

    cuarto commit

--- app/src/main/java/com/example/udmath/presentation/auth/login/LoginScreen.kt
+++ app/src/main/java/com/example/udmath/presentation/auth/login/LoginScreen.kt
@@ -1,5 +1,6 @@
 package com.example.udmath.presentation.auth.login
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,15 +9,19 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.unit.sp
+import com.example.udmath.ui.theme.Blue
+import com.example.udmath.ui.theme.DarkBlue
+import com.example.udmath.ui.theme.white
 
 @Composable
-fun LoginScreen(){
-    Column( modifier= Modifier.fillMaxSize(),horizontalAlignment = Alignment.CenterHorizontally) {
+fun LoginScreen( navigateToRegister: () -> Unit){
+    Column( modifier= Modifier.fillMaxSize().background(Brush.verticalGradient(listOf(Blue, DarkBlue))),horizontalAlignment = Alignment.CenterHorizontally) {
         Spacer(modifier = Modifier.weight(1f))
-        Text(text = "LOGIN SCREEN", fontSize = 25.sp)
+        Text(text = "LOGIN SCREEN", fontSize = 25.sp, color = white)
         Spacer(modifier = Modifier.weight(1f))
-        Button(onClick = {}) {
+        Button(onClick = {navigateToRegister()}) {
             Text(text = "Navegar al registro")
         }
         Spacer(modifier = Modifier.weight(1f))
--- app/src/main/java/com/example/udmath/presentation/auth/register/RegisterScreen.kt
+++ app/src/main/java/com/example/udmath/presentation/auth/register/RegisterScreen.kt
@@ -1,2 +1,34 @@
 package com.example.udmath.presentation.auth.register
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.unit.sp
+import com.example.udmath.presentation.components.AutoResizedText
+import com.example.udmath.ui.theme.Blue
+import com.example.udmath.ui.theme.DarkBlue
+
+@Composable
+fun RegisterScreen(){
+    Column( modifier= Modifier.fillMaxSize(),horizontalAlignment = Alignment.CenterHorizontally) {
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = "REGISTER SCREEN", fontSize = 25.sp)
+        Spacer(modifier = Modifier.weight(1f))
+        Button(onClick = {}) {
+            Text(text = "Navegar al login")
+        }
+        Spacer(modifier = Modifier.weight(1f))
+    }
+
+    AutoResizedText(
+        text = "REGISTRO"
+    )
+
+}
