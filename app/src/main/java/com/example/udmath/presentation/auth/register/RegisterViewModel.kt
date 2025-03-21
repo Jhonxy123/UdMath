32783fc2e9410f8450178e3cbef21e5476d35cf5
Jhon Gonzalez <jhonxy123@gmail.com>
2025-03-20 19:34:56 -0500

    comertarios jhon

--- app/src/main/java/com/example/udmath/presentation/auth/register/RegisterViewModel.kt
+++ app/src/main/java/com/example/udmath/presentation/auth/register/RegisterViewModel.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
+//inyección de dependecias con hilt
 class RegisterViewModel @Inject constructor(
     private val registerUserUseCase: RegisterUserUseCase
 ): ViewModel(){
