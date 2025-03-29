d7bf32f4650511147d97c549802ba407c1034351
Jhon Gonzalez <jhonxy123@gmail.com>
2025-03-29 13:21:11 -0500

    commit requerido

--- app/src/main/java/com/example/udmath/presentation/auth/menu/MenuViewModel.kt
+++ app/src/main/java/com/example/udmath/presentation/auth/menu/MenuViewModel.kt
@@ -1,4 +1,9 @@
 package com.example.udmath.presentation.auth.menu
 
-class MenuViewModel {
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MenuViewModel @Inject constructor() : ViewModel() {
 }
