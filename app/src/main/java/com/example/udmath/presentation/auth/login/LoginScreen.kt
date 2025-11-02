3dc3eb72726f2622e568f6ce8277105e3f8e3299
SantiAlfonso <salfonsop@udistrital.edu.co>
2025-11-02 16:02:44 -0500

    Diseño del NavigationDrawer del incio listo

--- app/src/main/java/com/example/udmath/presentation/auth/login/LoginScreen.kt
+++ app/src/main/java/com/example/udmath/presentation/auth/login/LoginScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.getValue//
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -30,9 +31,9 @@ import com.example.udmath.ui.theme.Black
 import com.google.firebase.auth.FirebaseAuth
 
 @Composable
-fun LoginScreen(auth: FirebaseAuth, viewModel: LoginViewModel = viewModel(),
-                navigateToRegister: () -> Unit, navigateToMenu: () -> Unit,
-                ){
+fun LoginScreen(auth: FirebaseAuth, viewModel: LoginViewModel = viewModel(), navigateToRegister: () -> Unit, navigateToMenu: () -> Unit)
+{
+
     val email by viewModel.email.collectAsStateWithLifecycle()
     val password by viewModel.password.collectAsStateWithLifecycle()
 
@@ -40,17 +41,26 @@ fun LoginScreen(auth: FirebaseAuth, viewModel: LoginViewModel = viewModel(),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         MyBanner(text="Bienvenido")
+
         Spacer(modifier = Modifier.height(20.dp))
+
         Text(text = "LOGIN SCREEN", fontSize = 25.sp, color = Black)
+
         //Spacer(modifier = Modifier.weight(1f))
         Spacer(modifier = Modifier.height(35.dp))
+
         emailText(email,{viewModel.onEmailChanged(it)}) //Campo de texto para el email del usuario
+
         Spacer(modifier = Modifier.height(20.dp))
+
         passwordText(password,{viewModel.onPasswordChanged(it)}) //Campo de texto para la contraseña del usuario
+
         Spacer(modifier = Modifier.height(48.dp))
 
         Button(
+
             onClick = {
+
                 auth.signInWithEmailAndPassword(email, password).addOnCompleteListener{ task ->
                     if(task.isSuccessful){
                         navigateToMenu()
@@ -60,13 +70,18 @@ fun LoginScreen(auth: FirebaseAuth, viewModel: LoginViewModel = viewModel(),
                         Log.i("aris", "LOGIN KO")
                     }
                 }
+
             }
+
         ) {
             Text(text = "Sign in")
         }
+
         Button(onClick = {navigateToRegister()}) {
             Text(text = "Navegar al registro")
         }
+
         Spacer(modifier = Modifier.weight(1f))
     }
+
 }
