fc2d43db3a9ac907129b0cd0902a86ddab42f5bd
Jhon Gonzalez <jhonxy123@gmail.com>
2026-03-06 22:15:33 -0500

    creo nuevo tipo de pregunta con opciones de imagenes

--- app/src/main/java/com/example/udmath/presentation/Recomendaciones/Components/TrueFalseQuestion.kt
+++ app/src/main/java/com/example/udmath/presentation/Recomendaciones/Components/TrueFalseQuestion.kt
@@ -3,21 +3,40 @@ package com.example.udmath.presentation.Recomendaciones.Components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
 fun TrueFalseQuestion(
     texto: String,
+    image: String,
     selectedOption: String?,
     enabled: Boolean,
     onSelect: (String) -> Unit
 ) {
     Column(verticalArrangement = Arrangement.spacedBy(10.dp)) {
 
-        // Opciones tipo botón para ahorrar espacio
+        if (image.isNotEmpty()) {
+            AsyncImage(
+                model = image,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(260.dp)
+                    .clip(RoundedCornerShape(10.dp)),
+                contentScale = ContentScale.Fit
+            )
+        }
+
         Row(horizontalArrangement = Arrangement.spacedBy(12.dp)) {
             listOf("Verdadero", "Falso").forEach { opt ->
                 val selected = selectedOption == opt
@@ -32,3 +51,4 @@ fun TrueFalseQuestion(
         }
     }
 }
+
